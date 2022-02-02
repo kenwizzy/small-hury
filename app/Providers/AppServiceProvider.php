bc0042c2bc439c3f644239b732aa473caae13b3f
Uzezi Jephter <42168298+jephterSmart@users.noreply.github.com>
2022-02-02 11:46:05 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,13 +22,13 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-//          if(env('REDIRECT_HTTPS'))
-//       {
-//         $url->forceScheme('https');
-//       }
+         if(env('REDIRECT_HTTPS'))
+      {
+        $url->forceScheme('https');
+      }
         
-        URL::forceScheme('https');
+        //URL::forceScheme('https');
     }
 }
