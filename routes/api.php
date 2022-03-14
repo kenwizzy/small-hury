013b7d3591458247b142f123d1ca5b3fbcd4eb94
Nwideh-Ken <43903882+kenwizzy@users.noreply.github.com>
2022-03-15 00:45:18 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\CartController;
-use App\Http\Controllers\API\UserController;
-use App\Http\Controllers\API\OrderController;
-use App\Http\Controllers\API\AddressController;
-use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\API\CategoryController;
-use App\Http\Controllers\API\WishlistController;
-use App\Http\Controllers\API\WareHouseController;
-use App\Http\Controllers\API\DeliveryCostController;
-use App\Http\Controllers\API\OrderAssigneeController;
-use App\Http\Controllers\API\WarehouseDistrictController;
-use App\Http\Controllers\OrderController as ControllersOrderController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,87 +16,5 @@ use App\Http\Controllers\OrderController as ControllersOrderController;
 |
 */
 
-Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-//Route::middleware(['auth:sanctum'])->group(function ($router) {
 
-Route::group([
-    'middleware' => 'auth:sanctum'
-], function ($router) {
-
-
-    //Route::get('/products', [ProductController::class, 'getProducts']);
-    Route::get('/categories', [CategoryController::class, 'getCategories']);
-    Route::get('/categories-parent',[CategoryController::class,'getParent']);
-    Route::get('/category/{id}', [CategoryController::class, 'show']);
-    Route::get('/product/{id}', [ProductController::class, 'show']);
-    Route::get('/districts', [WarehouseDistrictController::class, 'index']);
-
-    Route::get('/active_orders',[OrderAssigneeController::class,'active']);
-    Route::get('/pending_orders',[OrderAssigneeController::class,'pending']);
-    Route::get('/accept_order/{id}',[OrderAssigneeController::class,'accept']);
-    Route::get('/decline_order/{id}',[OrderAssigneeController::class,'decline']);
-    Route::get('/order_histories',[OrderAssigneeController::class,'history']);
-
-    Route::get('/order-details/{id}',[OrderAssigneeController::class,'show']);
-    /* Uzezi Jephter endpoints  */
-
-    //For working with users
-    Route::get('/user', [UserController::class, 'getUser']);
-    Route::post('update-user',[UserController::class,'update']);
-    Route::post('/logout',[AuthController::class,'logout']);
-    Route::post('/change-password',[UserController::class,'changePassword']);
-    Route::get('/unread-notification-count',[UserController::class,'getUnreadNotificationCount']);
-    Route::get('/notifications',[UserController::class,'getNotifications']);
-    Route::get('/read',[UserController::class,'readNotifications']);
-    Route::post('/enquiry',[UserController::class,'sendEnquiry']);
-    Route::post('/feedback',[UserController::class,'sendFeedback']);
-
-    Route::get('/warehouses',[WareHouseController::class,'index']);
-    Route::get('/warehouses/{id}',[WareHouseController::class,'show']);
-    //For managing addresses for user
-    Route::get('/addresses',[AddressController::class,'index']);
-    Route::get('/default-address',[AddressController::class,'getDefaultAddress']);
-    Route::post('/addresses',[AddressController::class,'create']);
-    Route::delete('/addresses/{id}',[AddressController::class,'destroy']);
-    Route::patch('/addresses/{id}',[AddressController::class,'edit']);
-
-    //For managing wishing of products
-    Route::patch('/wish-product/{id}',[ProductController::class,'wishProduct']);
-    Route::delete('/wish-product/{id}',[ProductController::class,'removeWished']);
-    Route::get('/search',[ProductController::class,'search']);
-    Route::get('/user-searches',[ProductController::class,'getRecentSearches']);
-    Route::get('/wishlists',[UserController::class,'getWishlists']);
-
-    //For adding Items to cart
-    Route::prefix('cart')->group(function(){
-        Route::get('/',[CartController::class,'index']);
-        Route::post('/add-product/{id}',[CartController::class,'addProduct']);
-        Route::post('/remove-product/{id}',[CartController::class,'removeProduct']);
-    });
-
-    // For geeting delivery cost
-    Route::get('delivery-cost',[DeliveryCostController::class,'index']);
-
-    //For dealing with Orders
-    Route::prefix('orders')->group(function(){
-        Route::get('/',[OrderController::class,'index']);
-        Route::post('/store',[OrderController::class,'store']);
-        Route::patch('/cancel/{id}',[OrderController::class,'cancelOrder']);
-        Route::put('/re-order/{id}',[OrderController::class,'reOrder']);
-        Route::get('/{id}',[OrderController::class,'show']);
-
-        Route::post('/rating',[OrderController::class,'storeRating']);
-        //This is a biker route
-        Route::get('/complete/{id}',[OrderController::class,'complete']);
-    });
-});
-
-Route::fallback(function () {
-    return response()->json([
-        'message' => 'Page Not Found. Please check your url and try again.'
-    ], 404);
 });
